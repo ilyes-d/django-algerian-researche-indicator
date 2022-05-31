c61d9d7714c8e896865ecf270a774f00e0f7d922
[merouanebj] <[merouane.mima@gmail.com]>
2022-05-31 15:50:50 +0100

    jjf

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -7,9 +7,16 @@ urlpatterns = [
          
     path('etablisement/' ,include([
         path('dashboard/' , chef_eta.dash_etablisemnt  , name='dashboard_etablisement'),
+        path('division/liste/' , chef_eta.dash_etablisemnt  , name='division_etablisement'),
+        path('equipe/liste/' , chef_eta.dash_etablisemnt  , name='equipe_etablisement'),
+        path('chercheur/card/' , chef_eta.dash_etablisemnt  , name='chercheur_card_etablisement'),
+        path('chercheur/list/' , chef_eta.dash_etablisemnt  , name='dashboard_etablisement'),
      ])),
      path('division/' ,include([
         path('dashboard/' , chef_div.dash_division  , name='dashboard_division'),
+        path('equipe/liste/' ,   chef_div.dash_etablisemnt  , name='equipe_division'),
+        path('chercheur/card/' , chef_div.dash_etablisemnt  , name='chercheur_card_division'),
+        path('chercheur/list/' , chef_div.dash_etablisemnt  , name='chercheur_list_division'),
      ])),
      path('equipe/' ,include([
         path('dashboard/' , chef_equipe.dash_equipe  , name='dashboard_equipe'),
