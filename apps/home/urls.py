f2aea54ec8bbdf620ba3c8391516dbeb7afb8924
ilyes-d <abdelkaderilyes.yahia@etu.usthb.dz>
2022-05-31 15:54:45 +0100

    modified urls

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -14,14 +14,10 @@ urlpatterns = [
      ])),
      path('division/' ,include([
         path('dashboard/' , chef_div.dash_division  , name='dashboard_division'),
-        path('equipe/liste/' ,   chef_div.dash_etablisemnt  , name='equipe_division'),
-        path('chercheur/card/' , chef_div.dash_etablisemnt  , name='chercheur_card_division'),
-        path('chercheur/list/' , chef_div.dash_etablisemnt  , name='chercheur_list_division'),
+        path('equipe/liste/' ,   chef_div.dash_division  , name='equipe_division'),
+        path('chercheur/card/' , chef_div.dash_division  , name='chercheur_card_division'),
+        path('chercheur/list/' , chef_div.dash_division  , name='chercheur_list_division'),
      ])),
-     path('equipe/' ,include([
-        path('dashboard/' , chef_equipe.dash_equipe  , name='dashboard_equipe'),
-     ])),
-    # re_path(r'^.*\.*', _views.pages, name='pages'),
     path('settings/', _views.settings , name= "settings"),
     path('try/' , _views.trying, name='try')
 ]
