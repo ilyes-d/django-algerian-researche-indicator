6a297fb522a39fef7e9406216e9ef93ad553043b
ilyes-d <abdelkaderilyes.yahia@etu.usthb.dz>
2022-06-18 16:04:15 +0100

    added some filters

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -67,6 +67,7 @@ def see_user_data(request):
     user.first_name = "ana howa"
     user.save()
     context['wilayas'] = Location.objects.all()
+    context['etas'] = Etablisment.objects.all()
     context["first_name"] = request.user.first_name
     context["userd"] = user
     return render(request , 'empty.html',context)
